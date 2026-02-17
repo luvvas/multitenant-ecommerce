5b0d1e7b1a7f71b10562eab22ffd7b5cb8a1ec02
Lucas Machado <lmachado72@outlook.com>
2026-02-17 18:07:37 -0300

    feat: sidebar buttons for mobile

--- src/app/(home)/navbar-sidebar.tsx
+++ src/app/(home)/navbar-sidebar.tsx
@@ -1,9 +1,11 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
     Sheet,
     SheetContent,
     SheetHeader,
     SheetTitle
 } from "@/components/ui/sheet";
+import Link from "next/link";
 
 interface NavbarItem {
     href: string
@@ -24,12 +26,38 @@ export const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
                 className="p-0 transition-none"
             >
                 <SheetHeader className="p-4 border-b">
-                    <div className="flex items-center">
-                        <SheetTitle>
-                            Menu
-                        </SheetTitle>
-                    </div>
+                    <SheetTitle>
+                        Menu
+                    </SheetTitle>
                 </SheetHeader>
+
+                <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+                    {items.map((item) => (
+                        <Link
+                            key={item.href}
+                            href={item.href}
+                            className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+                            onClick={() => onOpenChange(false)}
+                        >
+                            {item.children}
+                        </Link>
+                    ))}
+
+                    <div className="border-t">
+                        <Link
+                            href="/sign-in"
+                            className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            href="/sign-p"
+                            className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+                        >
+                            Start selling
+                        </Link>
+                    </div>
+                </ScrollArea>
             </SheetContent>
         </Sheet>
     )
