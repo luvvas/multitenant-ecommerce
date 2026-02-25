a39a7a0f0c1af95a44491641858cb1efca3f31d7
Lucas Machado <lmachado72@outlook.com>
2026-02-24 23:48:29 -0300

    feat: dynamic categories structure

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,49 @@
-import { Footer } from "./footer"
-import { Navbar } from "./navbar"
+import configPromise from "@payload-config";
+import { getPayload } from 'payload';
+
+import { Category } from "@/payload-types";
+import { Footer } from "./footer";
+import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
 
 interface Props {
     children: React.ReactNode
 }
 
-const layout = ({ children }: Props) => {
+const layout = async ({ children }: Props) => {
+    const payload = await getPayload({ config: configPromise })
+
+    const data = await payload.find({
+        collection: 'categories',
+        depth: 1, // Populate subcategories
+        pagination: false,
+        where: {
+            parent: {
+                exists: false
+            }
+        }
+    });
+
+    const formattedData = data.docs.map((doc: any) => {
+        return {
+            ...doc,
+            subcategories: (doc.subcategories?.docs ?? []).map((doc: Category) => ({
+                // Because of depth: 1
+                ...(doc as Category),
+                subcategories: undefined
+            })) || []
+        }
+    })
+
+    console.log({
+        data,
+        formattedData
+    })
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
+            <SearchFilters data={formattedData} />
             <div className="flex-1 bg-[#f4f4f0]">
                 {children}
             </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,16 +1,8 @@
-import configPromise from "@payload-config";
-import { getPayload } from 'payload';
-
 export default async function Home() {
-    const payload = await getPayload({ config: configPromise })
-
-    const data = await payload.find({
-        collection: 'categories',
-    });
 
     return (
         <div>
-            {JSON.stringify(data, null, 2)}
+            Home page
         </div>
     )
 }
